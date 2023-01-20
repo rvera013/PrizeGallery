99ad68f3f6d4b3026747c2c70e6a9879b9ab8029
rvera013 <99515590+rvera013@users.noreply.github.com>
2023-01-20 13:15:05 -0500

    Add files via upload

--- PrizeGallery/PrizeGallery/frmMain.vb
+++ PrizeGallery/PrizeGallery/frmMain.vb
@@ -18,6 +18,37 @@
         If lblResult.Text <> "" Then
             CalculateWinnings()
         End If
+    End Sub
+    Private Sub CalculateWinnings()
+        Dim intSpinVal As Integer
+        Dim intPriceNum As Integer
+        Dim sngPrizeVal As Single
+        Dim strImage As String
+        intSpinVal = CInt(lblResult.Text) 'convert the label's contents to an integer'
+        Select Case intSpinVal
+            Case Is < 100
+                intPriceNum = 1
+                sngPrizeVal = 50
+                strImage = "Prize-1.png"
+                picPrize.SizeMode = PictureBoxSizeMode.AutoSize
 
+            Case Is < 200
+                intPriceNum = 2
+                sngPrizeVal = 75
+                strImage = "Prize-2.png"
+                picPrize.SizeMode = PictureBoxSizeMode.AutoSize
+            Case Is < 300
+                intPriceNum = 3
+                sngPrizeVal = 60
+                strImage = "Prize-3.png"
+                picPrize.SizeMode = PictureBoxSizeMode.AutoSize
+            Case Else 'default. For any other value no in the case statement"
+                intPriceNum = 4
+                sngPrizeVal = 0
+                strImage = "consolation price.png"
+                picPrize.SizeMode = PictureBoxSizeMode.Zoom
+                lblForfeit.Visible = True
+
+        End Select
     End Sub
 End Class
