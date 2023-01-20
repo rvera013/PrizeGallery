22be08a47a44ba1f46ba4532ccf220b8fad77e21
rvera013 <99515590+rvera013@users.noreply.github.com>
2023-01-19 22:34:03 -0500

    Add files via upload

--- PrizeGallery/PrizeGallery/frmMain.Designer.vb
+++ PrizeGallery/PrizeGallery/frmMain.Designer.vb
@@ -23,23 +23,148 @@ Partial Class frmMain
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.Label1 = New System.Windows.Forms.Label()
+        Me.lblResult = New System.Windows.Forms.Label()
+        Me.lblWinnings = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.lblPriceVal = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btnStart = New System.Windows.Forms.Label()
+        Me.btnStop = New System.Windows.Forms.Label()
+        Me.btnQuit = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'Label1
         '
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
         Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(12, 30)
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(524, 77)
+        Me.Label1.Size = New System.Drawing.Size(776, 77)
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "Panther Price Gallery - Spin to Win Prices"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblResult
+        '
+        Me.lblResult.BackColor = System.Drawing.Color.Transparent
+        Me.lblResult.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblResult.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblResult.Location = New System.Drawing.Point(18, 66)
+        Me.lblResult.Name = "lblResult"
+        Me.lblResult.Size = New System.Drawing.Size(153, 45)
+        Me.lblResult.TabIndex = 1
+        Me.lblResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblWinnings
+        '
+        Me.lblWinnings.BackColor = System.Drawing.Color.White
+        Me.lblWinnings.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblWinnings.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblWinnings.Location = New System.Drawing.Point(18, 220)
+        Me.lblWinnings.Name = "lblWinnings"
+        Me.lblWinnings.Size = New System.Drawing.Size(153, 45)
+        Me.lblWinnings.TabIndex = 2
+        Me.lblWinnings.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'Label3
+        '
+        Me.Label3.BackColor = System.Drawing.Color.Transparent
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(21, 190)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(139, 30)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Your total winnings"
+        Me.Label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblPriceVal
+        '
+        Me.lblPriceVal.BackColor = System.Drawing.Color.White
+        Me.lblPriceVal.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblPriceVal.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblPriceVal.Location = New System.Drawing.Point(21, 141)
+        Me.lblPriceVal.Name = "lblPriceVal"
+        Me.lblPriceVal.Size = New System.Drawing.Size(150, 49)
+        Me.lblPriceVal.TabIndex = 4
+        Me.lblPriceVal.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'Label5
+        '
+        Me.Label5.BackColor = System.Drawing.Color.Transparent
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(18, 111)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(112, 30)
+        Me.Label5.TabIndex = 5
+        Me.Label5.Text = "Prize Value"
+        Me.Label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label2
+        '
+        Me.Label2.BackColor = System.Drawing.Color.Transparent
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.Red
+        Me.Label2.Location = New System.Drawing.Point(13, 265)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(139, 30)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "You Lose!"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'btnStart
+        '
+        Me.btnStart.BackColor = System.Drawing.Color.White
+        Me.btnStart.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.btnStart.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnStart.Location = New System.Drawing.Point(18, 304)
+        Me.btnStart.Name = "btnStart"
+        Me.btnStart.Size = New System.Drawing.Size(153, 31)
+        Me.btnStart.TabIndex = 7
+        Me.btnStart.Text = "Start the spin"
+        Me.btnStart.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'btnStop
+        '
+        Me.btnStop.BackColor = System.Drawing.Color.White
+        Me.btnStop.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.btnStop.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnStop.Location = New System.Drawing.Point(18, 344)
+        Me.btnStop.Name = "btnStop"
+        Me.btnStop.Size = New System.Drawing.Size(153, 31)
+        Me.btnStop.TabIndex = 8
+        Me.btnStop.Text = "Stop the spin"
+        Me.btnStop.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'btnQuit
+        '
+        Me.btnQuit.BackColor = System.Drawing.Color.White
+        Me.btnQuit.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.btnQuit.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnQuit.Location = New System.Drawing.Point(18, 385)
+        Me.btnQuit.Name = "btnQuit"
+        Me.btnQuit.Size = New System.Drawing.Size(153, 31)
+        Me.btnQuit.TabIndex = 9
+        Me.btnQuit.Text = "Quit"
+        Me.btnQuit.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         '
         'frmMain
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Linen
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnQuit)
+        Me.Controls.Add(Me.btnStop)
+        Me.Controls.Add(Me.btnStart)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.lblPriceVal)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.lblWinnings)
+        Me.Controls.Add(Me.lblResult)
         Me.Controls.Add(Me.Label1)
+        Me.ForeColor = System.Drawing.Color.DarkBlue
         Me.Name = "frmMain"
         Me.Text = "Mystery Prices"
         Me.ResumeLayout(False)
@@ -47,4 +172,13 @@ Partial Class frmMain
     End Sub
 
     Friend WithEvents Label1 As Label
+    Friend WithEvents lblResult As Label
+    Friend WithEvents lblWinnings As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents lblPriceVal As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents btnStart As Label
+    Friend WithEvents btnStop As Label
+    Friend WithEvents btnQuit As Label
 End Class
--- PrizeGallery/PrizeGallery/frmMain.vb
+++ PrizeGallery/PrizeGallery/frmMain.vb
@@ -2,4 +2,16 @@
     Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
 
     End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles lblResult.Click
+
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+
+    Private Sub Label4_Click(sender As Object, e As EventArgs) Handles btnQuit.Click
+
+    End Sub
 End Class
