ec4800367fc38678da7609b1e13ec8bbed8975a2
rvera013 <99515590+rvera013@users.noreply.github.com>
2023-01-20 09:53:41 -0500

    Add files via upload

--- PrizeGallery/PrizeGallery/frmMain.vb
+++ PrizeGallery/PrizeGallery/frmMain.vb
@@ -1,17 +1,6 @@
 ﻿Public Class frmMain
-    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+    Private IntResult As Integer
+    Private sngWinnings As Single
+    Private rndVal As New Random
 
-    End Sub
-
-    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles lblResult.Click
-
-    End Sub
-
-    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
-
-    End Sub
-
-    Private Sub Label4_Click(sender As Object, e As EventArgs) Handles btnQuit.Click
-
-    End Sub
 End Class
